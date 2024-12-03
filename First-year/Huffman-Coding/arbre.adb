b9900757a0440f427f46bebc390ddd2f82d24c49
Athos <athos.0day@protonmail.com>
2024-12-03 20:55:01 +0100

    Update arbre.adb

--- First-year/Huffman-Coding/arbre.adb
+++ First-year/Huffman-Coding/arbre.adb
@@ -107,8 +107,21 @@ package body ABR is
         --Cas où l'Arbre est vide 
         if Arbre=null then 
             null ;
-        elsif Arbre.all.Gauche/=null then 
+        end if ;
+
+        --Parcourt à gauche
+        if Arbre.all.Gauche/=null then 
             --On ajoute un 0 à la LCA associé à la valeur de l'id (mais ne sera pas utile) 
-            Enregistrer(Parcourt,
+            Enregistrer(Parcourt,Arbre.all.Identifiant,0) ;
+            Parcourt_Infixe(Arbre.all.Gauche, Liste);
+        end if;
+
+        --Parcourt à droite 
+        if Arbre.all.Droite/=null then 
+            --On ajoute un 1 à la LCA associé à la valeur de l'id (mais ne sera pas utile)
+            Enregistrer(Parcourt,Arbre.all.Identifiant,1) ;
+        end if;
+    end Parcourt_Infixe ;
+            
 
 
