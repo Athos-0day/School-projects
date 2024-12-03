f1d404929c8937c21610d42f16638b3f720873eb
Athos <athos.0day@protonmail.com>
2024-12-03 15:25:51 +0100

    Update arbre_exceptions.ads

--- First-year/Huffman-Coding/arbre_exceptions.ads
+++ First-year/Huffman-Coding/arbre_exceptions.ads
@@ -1,9 +1,9 @@
 -- Définition des exceptions pour le module ABR
 package ABR_Exceptions is
 
-	Arbre_Vide : Exception;	-- Cette exception est levée lorsqu'une opération sur un Arbre est tentée sur un arbre qui est vide
-  	Fils__Gauche_Vide : Exception; --Cette exception est levée lorsqu'une opération sur un Arbre est tentée sur un arbre dont le fils gauche est vide
-	Fils__Droit_Vide : Exception; --Cette exception est levée lorsqu'une opération sur un Arbre est tentée sur un arbre dont le fils droit est vide
+	Arbre_Vide : Exception;	-- Exception levée lorsqu'une opération sur un Arbre est tentée sur un arbre qui est vide
+  	Fils_Gauche_Vide : Exception; --Exception levée lorsqu'un fils gauche est null
+	Fils_Droit_Vide : Exception; --Exception levée lorsqu'un fils droit est null
 
 
 end ABR_Exceptions;
