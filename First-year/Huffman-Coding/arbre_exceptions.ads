334a0374ddf9042028202f5eea85aa4236cd7fd1
Athos <athos.0day@protonmail.com>
2024-12-03 15:18:53 +0100

    Update arbre_exceptions.ads

--- First-year/Huffman-Coding/arbre_exceptions.ads
+++ First-year/Huffman-Coding/arbre_exceptions.ads
@@ -1,1 +1,7 @@
+-- Définition des exceptions pour le module ABR
+package ABR_Exceptions is
 
+	Arbre_Vide_Exception  : Exception;	-- un Arbre est vide
+  Fils_Vide_Exception : Exception; --un Arbre ne possède pas de fils
+
+end ABR_Exceptions;
